5b49b31d4b6a176b9eae5c6da78f6c2e5780e22e
Kavithan13 <nagamkavitha1999@gmail.com>
2026-06-04 11:16:50 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+USERNAME = "admin"
+PASSWORD = "admin123"
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def validate():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username == USERNAME and password == PASSWORD:
+        return render_template('home.html', username=username)
+
+    return "Invalid Credentials"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
